9bc9427252ec4ccafc1e7574fc20b1e4d96ae75a
hanendyo.wicaksono <hanendyo.wicaksono@jatis.com>
2024-08-09 17:12:30 +0700

    Add unit testing

--- src/test/java/com/jatis/showcase/ShowcaseApplicationTests.java
+++ src/test/java/com/jatis/showcase/ShowcaseApplicationTests.java
@@ -1,6 +1,8 @@
 package com.jatis.showcase;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.jatis.showcase.dto.RegisterUserDto;
 import com.jatis.showcase.repository.UserRepository;
+import com.jatis.showcase.service.UserService;
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @ActiveProfiles("junit")
@@ -17,9 +21,17 @@ class ShowcaseApplicationTests {
 	@Autowired
 	UserRepository repo;
 	
+	@Autowired
+	UserService userService;
+	
 	@Test
 	void testUserRepository() {
-		assertNull(repo.findByEmail("test@test.com").orElse(null));
+		RegisterUserDto user = new RegisterUserDto();
+		user.setEmail("test@test.com");
+		user.setBirthDate(LocalDate.now());
+		user.setPassword("Password123");
+		userService.signup(user);
+		assertNotNull(repo.findByEmail("test@test.com").orElse(null));
 	}
 
 }
